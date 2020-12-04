1576b68886d4109fe779ef2c2ffd791f9d6fc368
Lewis <davanzo.lewis@gmail.com>
2020-12-04 11:50:05 -0500

    fixes FOUT

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,94 +1,102 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, onMount } from "svelte";
   import { slide } from "svelte/transition";
   const dispatch = createEventDispatcher();
   let open = false;
   let width;
+  //fixes FOUT
+  let ready;
+  onMount(() => {
+    ready = true;
+  });
+
   let sections = [
     { name: "Services", id: 1 },
     { name: "About", id: 2 },
     { name: "Contact", id: 3 },
   ];
+
   function sendScroll(name) {
-    if(width < 795) toggleOpen();
-    dispatch('sendScroll', name);
+    if (width < 795) toggleOpen();
+    dispatch("sendScroll", name);
   }
+
   function toggleOpen() {
     open = !open;
-    document.body.classList.toggle('open');
-  };
+    document.body.classList.toggle("open");
+  }
 </script>
 
 <svelte:window bind:innerWidth={width} />
-<nav>
-{#if width < 795}
-  {#if open}
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 30 30"
-      width="30"
-      height="30"
-      on:click={toggleOpen}
-      tabindex="0"
-      role="button"
-      aria-label="Menu"
-      aria-expanded="true"
-      on:keyup={(e) => e.key === 'Enter' && toggleOpen()}
-    >
-      <line x1="2" y1="2" x2="28" y2="28" stroke="#333" stroke-width="5" />
-      <line x1="28" y1="2" x2="2" y2="28" stroke="#333" stroke-width="5" />
-    </svg>
-  {:else}
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 30 30"
-      width="30"
-      height="30"
-      on:click={toggleOpen}
-      tabindex="0"
-      role="button"
-      aria-label="Menu"
-      aria-expanded="false"
-      on:keyup={(e) => e.key === 'Enter' && toggleOpen()}
-    >
-      <line x1="0" y1="5" x2="30" y2="5" stroke="#333" stroke-width="5" />
-      <line x1="0" y1="15" x2="30" y2="15" stroke="#333" stroke-width="5" />
-      <line x1="0" y1="25" x2="30" y2="25" stroke="#333" stroke-width="5" />
-    </svg>
-  {/if}
 
-  {#if open}
-    <ul transition:slide>
-      {#each sections as section (section.id)}
-        <li
-          on:click={() => sendScroll(section.name)}
+{#if ready}
+  <nav>
+    {#if width < 795}
+      {#if open}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 30 30"
+          width="30"
+          height="30"
+          on:click={toggleOpen}
           tabindex="0"
-          role="link"
-          on:keyup={(e) => e.key === 'Enter' && sendScroll(section.name)}
+          role="button"
+          aria-label="Menu"
+          aria-expanded="true"
+          on:keyup={(e) => e.key === 'Enter' && toggleOpen()}
         >
-          {section.name}
-        </li>
-      {/each}
-    </ul>
-  {/if}
-{:else}
-  <ul>
-    {#each sections as section (section.id)}
-      <li
-        on:click={() => sendScroll(section.name)}
-        tabindex="0"
-        role="link"
-        on:keyup={(e) => e.key === 'Enter' && sendScroll(section.name)}
-      >
-        {section.name}
-      </li>
-    {/each}
-  </ul>
+          <line x1="2" y1="2" x2="28" y2="28" stroke="#333" stroke-width="5" />
+          <line x1="28" y1="2" x2="2" y2="28" stroke="#333" stroke-width="5" />
+        </svg>
+
+        <ul transition:slide>
+          {#each sections as section (section.id)}
+            <li
+              on:click={() => sendScroll(section.name)}
+              tabindex="0"
+              role="link"
+              on:keyup={(e) => e.key === 'Enter' && sendScroll(section.name)}
+            >
+              {section.name}
+            </li>
+          {/each}
+        </ul>
+      {:else}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 30 30"
+          width="30"
+          height="30"
+          on:click={toggleOpen}
+          tabindex="0"
+          role="button"
+          aria-label="Menu"
+          aria-expanded="false"
+          on:keyup={(e) => e.key === 'Enter' && toggleOpen()}
+        >
+          <line x1="0" y1="5" x2="30" y2="5" stroke="#333" stroke-width="5" />
+          <line x1="0" y1="15" x2="30" y2="15" stroke="#333" stroke-width="5" />
+          <line x1="0" y1="25" x2="30" y2="25" stroke="#333" stroke-width="5" />
+        </svg>
+      {/if}
+    {:else}
+      <ul>
+        {#each sections as section (section.id)}
+          <li
+            on:click={() => sendScroll(section.name)}
+            tabindex="0"
+            role="link"
+            on:keyup={(e) => e.key === 'Enter' && sendScroll(section.name)}
+          >
+            {section.name}
+          </li>
+        {/each}
+      </ul>
+    {/if}
+  </nav>
 {/if}
-</nav>
 
 <style>
-  
   ul {
     height: 104vh;
     width: 100vw;
@@ -101,7 +109,7 @@
     position: absolute;
     top: -2.5rem;
     right: 0;
-    background-color:  #bacaec;
+    background-color: #bacaec;
     box-shadow: 5px 10px 10px gray;
   }
   li {
